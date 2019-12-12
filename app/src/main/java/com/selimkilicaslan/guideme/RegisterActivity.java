dc4d73a92f7e16cff9f200a3a14d69913c22605a
Selim Enes Kılıçaslan <33657157+SelimEnes@users.noreply.github.com>
2019-12-13 01:03:05 +0300

    .

--- app/src/main/java/com/selimkilicaslan/guideme/RegisterActivity.java
+++ app/src/main/java/com/selimkilicaslan/guideme/RegisterActivity.java
@@ -1,27 +1,55 @@
 package com.selimkilicaslan.guideme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+
+import java.io.File;
+import java.io.IOException;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    final int RESULT_LOAD_IMAGE = 1;
+
     EditText nameEditText, emailEditText, passwordEditText, phoneEditText;
     Button registerButton;
+    RadioGroup radioGroup;
+    RadioButton touristRadioButton, guideRadioButton;
 
     Spinner genderSpinner;
+    String Filename;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        nameEditText = findViewById(R.id.nameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        phoneEditText = findViewById(R.id.phoneEditText);
+        registerButton = findViewById(R.id.registerButton);
         genderSpinner = findViewById(R.id.genderSpinner);
+        radioGroup = findViewById(R.id.radioGroup);
+        touristRadioButton = findViewById(R.id.touristRadioButton);
+        guideRadioButton = findViewById(R.id.guideRadioButton);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Genders, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -32,4 +60,67 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void registerButtonOnClick(View view) {
     }
+
+    public void profileImageOnClick(View view) {
+        String[] galleryPermissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(galleryPermissions, 2);
+
+        } else {
+            Intent i = new Intent(
+                    Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+            startActivityForResult(i, RESULT_LOAD_IMAGE);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            Bitmap bitmap;
+            try {
+                bitmap = ImageHandler.handleSamplingAndRotationBitmap(getApplicationContext(), Uri.fromFile(new File(picturePath)));
+                Filename = picturePath.substring(picturePath.lastIndexOf("/") + 1);
+            } catch (IOException e) {
+                bitmap = null;
+                e.printStackTrace();
+            }
+            ImageView imageButton = findViewById(R.id.profileImageView);
+            imageButton.setImageBitmap(bitmap);
+            cursor.close();
+
+        }
+    }
+
 }
