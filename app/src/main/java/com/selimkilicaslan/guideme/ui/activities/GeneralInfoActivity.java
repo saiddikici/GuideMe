9896b97ff09f07926d1d2cdcb9e7bbe94bc650d3
Said <saiddikici@gmail.com>
2019-12-16 01:33:42 +0300

    ,

--- app/src/main/java/com/selimkilicaslan/guideme/ui/activities/GeneralInfoActivity.java
+++ app/src/main/java/com/selimkilicaslan/guideme/ui/activities/GeneralInfoActivity.java
@@ -16,15 +16,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.selimkilicaslan.guideme.R;
 import com.selimkilicaslan.guideme.classes.ImageHandler;
 import com.selimkilicaslan.guideme.classes.MyAppCompatActivity;
+import com.selimkilicaslan.guideme.classes.User;
 
 import java.io.File;
 import java.io.IOException;
@@ -35,8 +45,8 @@ public class GeneralInfoActivity extends MyAppCompatActivity {
     final int RESULT_LOAD_IMAGE = 1;
 
     ImageView generalInfoProfileImageView;
+    TextView emailTextView;
     EditText nameEditText;
-    EditText emailEditText;
     EditText phoneEditText;
     EditText currentPasswordEditText;
     EditText newPasswordEditText;
@@ -45,6 +55,8 @@ public class GeneralInfoActivity extends MyAppCompatActivity {
 
     String filePath;
 
+    User user;
+
     UserProfileChangeRequest profileUpdates;
 
     @Override
@@ -52,33 +64,44 @@ public class GeneralInfoActivity extends MyAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_info);
 
+
         generalInfoProfileImageView = findViewById(R.id.generalInfoProfileImageView);
+        emailTextView = findViewById(R.id.emailTextView);
         nameEditText = findViewById(R.id.nameEditText);
-        emailEditText = findViewById(R.id.emailEditText);
         phoneEditText = findViewById(R.id.phoneEditText);
         currentPasswordEditText = findViewById(R.id.currentPasswordEditText);
         newPasswordEditText = findViewById(R.id.newPasswordEditText);
         confirmNewPasswordEditText = findViewById(R.id.confirmNewPasswordEditText);
         saveProfileChangesButton = findViewById(R.id.saveProfileChangesButton);
 
+
+        DocumentReference docRef = mDatabase.collection("users").document(mUser.getUid());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                user = documentSnapshot.toObject(User.class);
+
+                /*nameEditText.setText(user.getUsername());
+                emailTextView.setText(user.getEmail());
+                phoneEditText.setText(user.getPhoneNumber());*/
+            }
+        });
+
         Glide.with(generalInfoProfileImageView)
                 .load(mUser.getPhotoUrl())
                 .into(generalInfoProfileImageView);
         nameEditText.setText(mUser.getDisplayName());
-        emailEditText.setText(mUser.getEmail());
+        emailTextView.setText(mUser.getEmail());
         phoneEditText.setText(mUser.getPhoneNumber());
-
-
-
     }
 
-    //TODO DATABASE EKLE
+    //TODO şifreleri kontrol et
+    //TODO email güncellemeyi şeyet
+    //TODO Profil resmi güncellemeyi kontrol et
     public void saveButtonOnClick(View view) {
-        if (!nameEditText.getText().equals(mUser.getDisplayName())){
-            profileUpdates = new UserProfileChangeRequest.Builder()
-                    .setDisplayName(nameEditText.getText().toString())
-                    .build();
-        }
+        profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(nameEditText.getText().toString())
+                .build();
         mUser.updateProfile(profileUpdates)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
@@ -93,6 +116,32 @@ public class GeneralInfoActivity extends MyAppCompatActivity {
                         }
                     }
                 });
+        AuthCredential credential = EmailAuthProvider.getCredential(emailTextView.getText().toString(), currentPasswordEditText.getText().toString());
+        mUser.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            mUser.updatePassword(newPasswordEditText.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Log.d("GuideMe", "Password Updated");
+                                        Toast.makeText(GeneralInfoActivity.this, "Password Updated", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+                                        Log.d("GuideMe", "Error");
+                                        Toast.makeText(GeneralInfoActivity.this, "Error, password not updated.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            Log.d("GuideMe", "Auth failed");
+                            Toast.makeText(GeneralInfoActivity.this, "Auth failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 
     public void profileImageOnClick(View view) {
